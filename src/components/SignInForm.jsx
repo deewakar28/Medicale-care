0343a9f532ced32badab266f4c16984c0395f977
Satya12 <satyaprakashjena492@gmail.com>
2023-10-26 19:13:40 +0530

    Validation in sign in form using formik

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -3,15 +3,30 @@ import { auth } from '../config/firebase'
 import {signInWithEmailAndPassword} from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
 import SignUp from "../assets/SignUp.png"
+import {useFormik} from 'formik';
+import {signUpSchema} from '../schemas'
 
 const SignInForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    //const [email, setEmail] = useState("");
+   // const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    const singIn = async()=>{
-      try{
-          await signInWithEmailAndPassword(auth,email, password)
+    const initialValues = {
+      email: "",
+      password: "",
+    };
+
+    const { values, errors, touched, handleBlur, handleChange, handleSubmit }=useFormik({
+      initialValues:initialValues,
+      validationSchema:signUpSchema,
+      onSubmit: async(values, action) => {
+        console.log(
+          "🚀 ~ file: Registration.jsx ~ line 11 ~ Registration ~ values",
+          values
+        );
+        action.resetForm();
+        try{
+          await signInWithEmailAndPassword(auth,values.email, values.password)
           .then((userCredential)=>{
             const user = userCredential.user;
             console.log(user);
@@ -28,7 +43,30 @@ const SignInForm = () => {
         console.log(error);
       }
     
-  }
+        
+      },
+    });
+
+  //   const singIn = async()=>{
+  //     try{
+  //         await signInWithEmailAndPassword(auth,email, password)
+  //         .then((userCredential)=>{
+  //           const user = userCredential.user;
+  //           console.log(user);
+  //           setTimeout(()=>{
+  //             alert(user.email+" Login Successfully!!");
+  //             document.getElementById('signup').style.display = 'none';     
+  //             document.getElementById('logout').style.display = 'block';
+  //           },1000)
+  //         })
+  //         navigate("/"); 
+                 
+  //     }
+  //     catch(error){
+  //       console.log(error);
+  //     }
+    
+  // }
   return (
     <div class="bg-cyan-500 min-h-screen lg:flex ">
              <div className='lg:h-[100vh] h-auto lg:w-[50vw] w-full flex items-center justify-center'>
@@ -40,7 +78,8 @@ const SignInForm = () => {
             <div class="lg:W-[49vw]   flex flex-col items-center justify-center px-2">
                 <div class=" px-6 lg:py-9  text-black ">
                     <h1 class="mb-8 text-3xl text-center">Sign In</h1>
-                    <label className="text-black sm:text-2xl text-[18px] tracking-wide">
+                   <form onSubmit={handleSubmit}>
+                   <label className="text-black sm:text-2xl text-[18px] tracking-wide">
                     Email
                   </label>
                    <div className=' border-b border-black mb-4'>
@@ -48,11 +87,13 @@ const SignInForm = () => {
                         type="text"
                         class="lg:w-[35vw] w-[80vw] bg-transparent h-[35px]"
                         name="email"
-                        value={email}
-                        onChange={e=>{
-                          setEmail(e.target.value)
-                        }}
-                       />
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        autoComplete='off'
+                       />{errors.email && touched.email ? (
+                        <p className="form-error text-red-600">{errors.email}</p>
+                      ) : null}
                     </div>
                     <label htmlFor="password" className="text-black sm:text-2xl text-[18px] tracking-wide max-w-[289px] pt-4 max-md:mt-10">
                     Password
@@ -62,17 +103,19 @@ const SignInForm = () => {
                         type="password"
                         class="lg:w-[35vw] w-[80vw] bg-transparent h-[35px]"
                         name="password"
-                        value={password}
-                        onChange={e=>{
-                          setPassword(e.target.value)
-                        }}
-                     />
+                        autoComplete='off'
+                        value={values.password}
+                        onChange={handleChange}
+                       onBlur={handleBlur}
+                     />{errors.password && touched.password ? (
+                      <p className="form-error text-red-600">{errors.password}</p>
+                    ) : null}
                       </div>
                     <button
-                        onClick={singIn}
-                        type="submit"
+                        //type="submit"
                         className="text-cyan-500 text-opacity-90 text-xl font-medium  bg-black  p-3 rounded-2xl hover:scale-95 lg:w-[35vw] w-[80vw]"
                     >Log In</button>
+                   </form>
                 </div>
 
                 <div class="text-grey-dark mt-6 mb-10 md:text-2xl">
