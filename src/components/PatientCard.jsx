9e1997f6dfd16a364a3bff6569330ec4ff282307
AviralPatel0526 <aviralpatel2603@gmail.com>
2023-10-27 17:59:09 +0530

    added open report functionality

--- src/components/PatientCard.jsx
+++ src/components/PatientCard.jsx
@@ -1,30 +1,79 @@
-import React from 'react'
-import patientImg from "../assets/patient.png"
-import { db } from '../config/firebase'
-import { deleteDoc, doc } from 'firebase/firestore'
+
+import React, { useState } from 'react';
+import patientImg from "../assets/patient.png";
+import { db } from '../config/firebase';
+import { deleteDoc, doc, getDoc } from 'firebase/firestore';
+import Rport from './Report';
 
 function PatientCard(prop) {
+  const [showReport, setShowReport] = useState(false);
+  const [filteredData, setFilteredData] = useState(null);
 
-  const deltePatientInfo = async(id)=>{
-    const patientDoc = doc(db, "PatientsList", id)
-    await deleteDoc(patientDoc)
+  const openReport = async (id) => {
+    const docRef = doc(db, 'PatientsList', id);
+
+    try {
+      const data = await getDoc(docRef);
+      const filteredData = data.data();
+      setFilteredData(filteredData);
+      setShowReport(true);
+    } catch (err) {
+      console.error(err);
+    }
   }
+
+  const deletePatientInfo = async (id) => {
+    const patientDoc = doc(db, "PatientsList", id);
+    await deleteDoc(patientDoc);
+  }
+
+  const closeReport = () => {
+    setShowReport(false);
+  }
+
   return (
-   <>
-   <div className='w-[90vw] bg-gray-300 lg:flex-row lg:h-[9vh] mx-auto rounded-2xl justify-between items-center px-2 flex flex-col gap-4 py-4 lg:py-0'>
-    <div className='h-[9vh] '><img className='h-[9vh]' src={patientImg} alt="" /></div>
-    <div className='min-w-[250px] text-center'><p>{prop.Name}</p></div>
-    <div><p>{prop.phone}</p></div>
-    <div><p>{prop.bloodGroup}</p></div>
-    <div><p>{prop.date}</p></div> 
-    <div><p>{prop.status}</p></div>
-    <div><button onClick={()=>{
-      deltePatientInfo(prop.id)
-      }} className='lg:p-1 px-20 py-2 rounded-2xl bg-gray-600 hover:bg-gray-500 hover:scale-95'>Delete</button></div> 
-    <a href="/PatientReport"><div><button className='lg:p-1 px-20 py-2 rounded-2xl bg-gray-600 hover:bg-gray-500 hover:scale-95'>Open</button></div> </a>
-   </div>
-   </>
+    <>
+      <div className='w-[90vw] bg-gray-300 lg:flex-row lg:h-[9vh] mx-auto rounded-2xl justify-between items-center px-2 flex flex-col gap-4 py-4 lg:py-0'>
+        <div className='h-[9vh] '><img className='h-[9vh]' src={patientImg} alt="" /></div>
+        <div className='min-w-[250px] text-center'><p>{prop.Name}</p></div>
+        <div><p>{prop.phone}</p></div>
+        <div><p>{prop.bloodGroup}</p></div>
+        <div><p>{prop.date}</p></div> 
+        <div><p>{prop.status}</p></div>
+        <div><button onClick={()=>{
+      deletePatientInfo(prop.id)
+      }} className='lg:p-1 px-20 py-2 rounded-2xl bg-gray-600 hover:bg-gray-500 hover:scale-95'>Delete</button></div>
+        <div>
+          <button
+            className='lg:p-1 px-20 py-2 rounded-2xl bg-gray-600 hover:bg-gray-500 hover:scale-95'
+            onClick={() => openReport(prop.id)}
+          >
+            Open
+          </button>
+        </div>
+      </div>
+
+      {showReport && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="modal fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+            <div className="modal-content bg-white p-4 rounded-lg">
+              <span className="close absolute top-3 right-3 text-2xl cursor-pointer" onClick={closeReport}>&times;</span>
+              <Rport
+                name={filteredData.Name}
+                gender={filteredData.Gender}
+                phone={filteredData.PhoneNumber}
+                date={filteredData.Data}
+                bloodGroup={filteredData.BloodGroup}
+                medicine={filteredData.Medicine}
+                problem={filteredData.Problem}
+                status={filteredData.Status}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
-export default PatientCard
+export default PatientCard;
--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/logo.png"
 function Report(prop) {
 return (
    <>
-   <div className='w-full h-[100vh] bg-cyan-500 flex flex-col gap-5 justify-center items-center'>
-     <div><h1 className='text-4xl text-bold text-white'>Patients Report</h1></div>
+   <div className='w-[100vw] h-[100vh] bg-cyan-500 flex flex-col gap-5 justify-center items-center'>
+     <div><h1 className='text-4xl text-bold text-white'>Patient's Report</h1></div>
     <div className='md:w-[60vw] w-[90vw] md:h-[70vh] h-auto bg-white rounded-2xl'>
         <div className='w-[60vw] h-[10vh] flex items-center'>
          <img src={logo} alt="" className='h-[10vh]'/>
@@ -15,20 +15,20 @@ return (
            <div className='flex lg:flex-row flex-col
             md:w-[50vw] justify-between'>
             <div className='flex flex-col gap-2 py-2'>
-            <p>{prop.name}</p> 
-            <p>{prop.gender}</p>      
-            <p>{prop.phone}</p>
+            <p>Name: {prop.name}</p> 
+            <p>Gender: {prop.gender}</p>      
+            <p>Phone: {prop.phone}</p>
             </div>
             <div className='flex flex-col gap-2 '>
-            <p>{prop.date}</p>
-            <p>{prop.bloodGroup}</p>
-            <p>{prop.medicine}</p>
+            <p>Date: {prop.date}</p>
+            <p>Blood-Group: {prop.bloodGroup}</p>
+            <p>Medicine: {prop.medicine}</p>
             </div>
            </div>
            <div>
-            <p>{prop.problem}</p>
+            <p>Problem: {prop.problem}</p>
            </div>
-           <div>{prop.status}</div>
+           <div>Status: {prop.status}</div>
         </div>
     </div>
     </div>
